216f0112d3718696e6ba95d6670cfaa06fc11e12
Pavel Kovalyov <sxe.funky@gmail.com>
2013-04-02 18:15:58 +0300

    implemented: tab for merchandising section to mass apply/dismiss promo prices

--- Promo.php
+++ Promo.php
@@ -11,6 +11,11 @@ class Promo extends Tools_Plugins_Abstract {
 		'shopping'
 	);
 
+	/**
+	 * @var Zend_Controller_Action_Helper_Json
+	 */
+	protected $_jsonHelper;
+
 	protected function _init() {
 		$missedPlugins = array_diff($this->_dependsOn, Tools_Plugins_Tools::getEnabledPlugins(true));
 
@@ -74,6 +79,58 @@ class Promo extends Tools_Plugins_Abstract {
 		}
 
 		echo $this->_view->render('tab.phtml');
+	}
+
+	public function merchandisingAction(){
+		$cacheHelper = Zend_Controller_Action_HelperBroker::getStaticHelper('cache');
+		if ($this->_request->isPost()){
+			$promoTable = new Zend_Db_Table('plugin_promo');
+			if ($this->_request->has('dismiss')){
+				try {
+					$result = $promoTable->delete('product_id IS NOT NULL');
+					$this->_responseHelper->success('Updated '.$result.' products');
+				} catch (Exception $e){
+					$this->_responseHelper->fail('Error');
+				}
+			} else {
+				try {
+					$discount = filter_var($this->_request->getParam('promo-price'), FILTER_VALIDATE_FLOAT);
+					$promoFrom = filter_var($this->_request->getParam('promo-from'), FILTER_SANITIZE_STRING);
+					$promoDue = filter_var($this->_request->getParam('promo-due'), FILTER_SANITIZE_STRING);
+
+					if (is_numeric($discount)){
+						$discount = floatval($discount);
+						if ($discount > 100 || $discount < 0){
+							$this->_responseHelper->fail($this->_translator->translate('Sales discount should be between 0 and 100 percents'));
+						}
+					} else {
+						$this->_responseHelper->fail($this->_translator->translate('Sale discount should be a number'));
+					}
+
+					$dateValidator = new Zend_Validate_Date(array('format' => 'd-M-Y'));
+					if ($dateValidator->isValid($promoFrom)){
+						$promoFrom = date(Tools_System_Tools::DATE_MYSQL, strtotime($promoFrom));
+					} else {
+						$this->_responseHelper->fail($this->_translator->translate('Wrong date format'));
+					}
+					if ($dateValidator->isValid($promoDue)){
+						$promoDue = date(Tools_System_Tools::DATE_MYSQL, strtotime($promoDue));
+					} else {
+						$this->_responseHelper->fail($this->_translator->translate('Wrong date format'));
+					}
+
+					$sql = 'INSERT INTO `plugin_promo` SELECT id as product_id, ROUND(price-price*%1$d/100, 2) as promo_price, \'%2$s\' as promo_from, \'%3$s\' as promo_due FROM `shopping_product` ON DUPLICATE KEY UPDATE promo_price = ROUND(price-price*%1$d/100, 2), promo_from = \'%2$s\', promo_due = \'%3$s\'';
+					$sql = sprintf($sql, $discount, $promoFrom, $promoDue);
+					$promoTable->getAdapter()->query($sql);
+					$cacheHelper->clean(false, false, array('product_price'));
+					$this->_responseHelper->success($this->_translator->translate('All products were updated'));
+				} catch (Exception $e){
+					$this->_responseHelper->fail($e->getMessage());
+				}
+			}
+			$this->_responseHelper->response('Bad request', true, 400);
+		}
+		echo $this->_view->render('merchandisingTab.phtml');
 	}
 
 	protected function _makeOptionPrice() {
