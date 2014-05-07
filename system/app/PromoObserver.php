aa6b2e52372a7f6b937e9e39efeb5d66710cbddd
Pavel Kovalyov <sxe.funky@gmail.com>
2014-05-07 11:48:59 +0300

    Update extra attribute with tax applied

--- system/app/PromoObserver.php
+++ system/app/PromoObserver.php
@@ -1,39 +1,51 @@
 <?php
+
 /**
  * PromoObserver
  * @author Pavel Kovalyov <pavlo.kovalyov@gmail.com>
  */
-class PromoObserver implements Interfaces_Observer {
+class PromoObserver implements Interfaces_Observer
+{
 
-	private $_dbTable;
+    private $_dbTable;
 
-	private static $_configParams = null;
+    private static $_configParams = null;
 
-	public function __construct() {
-		if (self::$_configParams === null) {
-			self::$_configParams = Models_Mapper_ShoppingConfig::getInstance()->getConfigParams();
-		}
-		$this->_dbTable = new Promo_DbTables_PromoDbTable();
-	}
+    public function __construct()
+    {
+        if (self::$_configParams === null) {
+            self::$_configParams = Models_Mapper_ShoppingConfig::getInstance()->getConfigParams();
+        }
+        $this->_dbTable = new Promo_DbTables_PromoDbTable();
+    }
 
 
-	/**
-	 * @param $object Models_Model_Product
-	 */
-	public function notify($object) {
-		$row = $this->_dbTable->find($object->getId())->current();
-		if ($row !== null) {
-			$now = time();
-			if (strtotime($row->promo_due) < $now) {
-				$row->delete();
-			} elseif (strtotime($row->promo_from) < $now) {
-				$object->setCurrentPrice($row->promo_price);
-				$object->addExtraProperty(array(
-					'g:sale_price'                => $row->promo_price . ' ' . self::$_configParams['currency'],
-					'g:sale_price_effective_date' => date(DATE_ATOM, strtotime($row->promo_from)) . '/' . date(DATE_ATOM, strtotime($row->promo_due))
-				));
-			}
-		}
-	}
+    /**
+     * @param $object Models_Model_Product
+     */
+    public function notify($object)
+    {
+        $row = $this->_dbTable->find($object->getId())->current();
+        if ($row !== null) {
+            $now = time();
+            if (strtotime($row->promo_due) < $now) {
+                $row->delete();
+            } elseif (strtotime($row->promo_from) < $now) {
+                $object->setCurrentPrice($row->promo_price);
+                $object->addExtraProperty(
+                    array(
+                        'g:sale_price' => Tools_ShoppingCart::getInstance()->calculateProductPrice(
+                                $object,
+                                $object->getDefaultOptions()
+                            ) . ' ' . self::$_configParams['currency'],
+                        'g:sale_price_effective_date' => date(DATE_ATOM, strtotime($row->promo_from)) . '/' . date(
+                                DATE_ATOM,
+                                strtotime($row->promo_due)
+                            )
+                    )
+                );
+            }
+        }
+    }
 
 }
