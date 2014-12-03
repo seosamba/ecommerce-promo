51cd8233632c1fe2b58723ee28170da00b6254b3
andreybudchenko <andrei@seosamba.com>
2014-12-03 12:40:53 +0200

    Fix for not existing product

--- system/app/PromoObserver.php
+++ system/app/PromoObserver.php
@@ -26,56 +26,58 @@ class PromoObserver implements Interfaces_Observer
     public function notify($object)
     {
         $prodId = $object->getId();
-        $promoConfig = $this->_dbTable->getAllPromoConfigData($prodId);
-        if(!empty($promoConfig)){
-            $currentPromo = array_shift($promoConfig);
-            $now = time();
-            if (strtotime($currentPromo['promo_due']) < $now) {
-                if ($currentPromo['scope'] === 'global') {
-                    $promoGlobalConfigTable = new Zend_Db_Table('plugin_promo_main_config');
-                    $promoGlobalConfigTable->delete('id IS NOT NULL');
+        if($prodId){
+            $promoConfig = $this->_dbTable->getAllPromoConfigData($prodId);
+            if(!empty($promoConfig)){
+                $currentPromo = array_shift($promoConfig);
+                $now = time();
+                if (strtotime($currentPromo['promo_due']) < $now) {
+                    if ($currentPromo['scope'] === 'global') {
+                        $promoGlobalConfigTable = new Zend_Db_Table('plugin_promo_main_config');
+                        $promoGlobalConfigTable->delete('id IS NOT NULL');
+                    }
+                } elseif (strtotime($currentPromo['promo_from']) < $now) {
+                    $currentPrice = $object->getCurrentPrice();
+                    if (empty($currentPrice)) {
+                        $currentPrice = $object->getPrice();
+                    }
+
+                    $currentPromo['sign'] = 'minus';
+                    $priceWithPromo = Tools_DiscountTools::applyDiscountData($currentPrice, $currentPromo);
+                    $object->setCurrentPrice($priceWithPromo);
+
+                    $productDiscounts = $object->getProductDiscounts();
+                    array_push(
+                        $productDiscounts,
+                        array(
+                            'name' => 'promo',
+                            'discount' => $currentPromo['discount'],
+                            'type' => $currentPromo['type'],
+                            'sign' => $currentPromo['sign']
+                        )
+                    );
+                    $object->setProductDiscounts($productDiscounts);
+
+
+                    $salePrice = number_format(
+                        Tools_ShoppingCart::getInstance()->calculateProductPrice(
+                            $object,
+                            $object->getDefaultOptions()
+                        ),
+                        2,
+                        '.',
+                        ''
+                    );
+                    $object->addExtraProperty(
+                        array(
+                            'g:sale_price'                => $salePrice . ' ' . self::$_configParams['currency'],
+                            'g:sale_price_effective_date' => date(DATE_ATOM, strtotime($currentPromo['promo_from'])) . '/' . date(
+                                    DATE_ATOM,
+                                    strtotime($currentPromo['promo_due'])
+                                )
+                        )
+                    );
                 }
-            } elseif (strtotime($currentPromo['promo_from']) < $now) {
-                $currentPrice = $object->getCurrentPrice();
-                if (empty($currentPrice)) {
-                    $currentPrice = $object->getPrice();
-                }
-
-                $currentPromo['sign'] = 'minus';
-                $priceWithPromo = Tools_DiscountTools::applyDiscountData($currentPrice, $currentPromo);
-                $object->setCurrentPrice($priceWithPromo);
-
-                $productDiscounts = $object->getProductDiscounts();
-                array_push(
-                    $productDiscounts,
-                    array(
-                        'name' => 'promo',
-                        'discount' => $currentPromo['discount'],
-                        'type' => $currentPromo['type'],
-                        'sign' => $currentPromo['sign']
-                    )
-                );
-                $object->setProductDiscounts($productDiscounts);
-
-
-                $salePrice = number_format(
-                    Tools_ShoppingCart::getInstance()->calculateProductPrice(
-                        $object,
-                        $object->getDefaultOptions()
-                    ),
-                    2,
-                    '.',
-                    ''
-                );
-                $object->addExtraProperty(
-                    array(
-                        'g:sale_price'                => $salePrice . ' ' . self::$_configParams['currency'],
-                        'g:sale_price_effective_date' => date(DATE_ATOM, strtotime($currentPromo['promo_from'])) . '/' . date(
-                                DATE_ATOM,
-                                strtotime($currentPromo['promo_due'])
-                            )
-                    )
-                );
             }
         }
     }
